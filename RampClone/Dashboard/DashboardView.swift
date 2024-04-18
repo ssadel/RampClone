1e14b1ae25b75fbaf7686ad3ac9d111268e87e3d
Sid Sadel <sidneysadel@gmail.com>
2024-04-18 12:59:00 -0400

    utils + dashboard view

---
+++ RampClone/Dashboard/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  RampClone
+//
+//  Created by Sidney Sadel on 4/18/24.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
---
+++ RampClone/Utils/Color+Ex.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Ex.swift
+//  RampClone
+//
+//  Created by Sidney Sadel on 4/18/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let sand = Color(#colorLiteral(red: 0.96, green: 0.95, blue: 0.94, alpha: 1.00))
+    static let neonYellow = Color(#colorLiteral(red: 0.91, green: 0.95, blue: 0.12, alpha: 1.00))
+}
