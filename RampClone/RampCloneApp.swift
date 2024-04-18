cd065f22455e508ec0cb93a390b49626831888ce
Sid Sadel <sidneysadel@gmail.com>
2024-04-18 12:42:50 -0400

    Initial Commit

---
+++ RampClone/RampCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RampCloneApp.swift
+//  RampClone
+//
+//  Created by Sidney Sadel on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RampCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
